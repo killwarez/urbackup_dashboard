8659b4ba661b130b418a9e278869d3500a9e3614
John Smith <you@example.com>
2023-05-15 13:57:24 +0200

    Fixed upload shell script

--- export_clients.sh
+++ export_clients.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-HOST = '192.168.1.1'
-USER = '12345'
-PASS = '12345'
+HOST='192.168.1.1'
+USER='12345'
+PASS='12345'
 sqlite3 -header -csv /var/urbackup/backup_server.db "select * from clients" >/tmp/clients_$HOSTNAME.csv
 ftp -n $HOST <<FTP_SCRIPT
 quote user $USER
